8ae66743a4c6bfa7eedfad99465dca0f26d1f7a1
Shaik <ameeruns@f45c89abcadd.ant.amazon.com>
2020-04-22 18:20:23 -0700

    initialfile

---
+++ math_series/series.py
@@ -0,0 +1,14 @@
+
+def fibonacci(n): 
+ if n < 0: 
+  print("Incorrect input") 
+# First Fibonacci number is 0 
+ elif n==1: 
+    return 0
+# Second Fibonacci number is 1 
+ elif n==2: 
+   return 1
+ else: 
+   return fibonacci(n-1)+fibonacci(n-2) 
+# Driver function 
+print(fibonacci(4))
