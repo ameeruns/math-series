c50d0bbc60ddb8a6776dc7d44eef2e0dc99ddc03
Shaik <ameeruns@f45c89abcadd.ant.amazon.com>
2020-04-23 19:34:29 -0700

    PushTheChange

--- math_series/series.py
+++ math_series/series.py
@@ -1,14 +1,32 @@
 
-def fibonacci(n): 
- if n < 0: 
+def fibonacci(input_num): 
+ if input_num < 0: 
   print("Incorrect input") 
 # First Fibonacci number is 0 
- elif n==1: 
+ elif input_num==1: 
     return 0
 # Second Fibonacci number is 1 
- elif n==2: 
+ elif input_num==2: 
    return 1
  else: 
-   return fibonacci(n-1)+fibonacci(n-2) 
-# Driver function 
-print(fibonacci(4))
+   return fibonacci(input_num-1)+fibonacci(input_num-2) 
+
+def lucas(input_num): 
+ if input_num < 0: 
+  print("Incorrect input") 
+# First Lucas number is 2
+ elif input_num==1: 
+    return 2
+# Second Lucas number is 1 
+ elif input_num==2: 
+   return 1
+ else: 
+   return lucas(input_num-1)+lucas(input_num-2) 
+
+def sum_series(num_check,optional_one=0,optional_two=1):
+    if optional_one == 0 and optional_two == 1:
+       return fibonacci(num_check)
+    elif optional_one == 2 and optional_two == 1:
+       return lucas(num_check)
+    else:
+        print("Incorrect parameter") 
--- tests/test_math_series.py
+++ tests/test_math_series.py
@@ -1,8 +1,103 @@
 from math_series import __version__ 
 from math_series.series import fibonacci
+from math_series.series import lucas
+from math_series.series import sum_series
+
 def test_version():
     assert __version__ == "0.1.0"
+def test_fibonacci_one():
+    actual = fibonacci(1)
+    expected = 0
+    assert actual == expected
 def test_fibonacci_two():
     actual = fibonacci(2)
     expected = 1
+    assert actual == expected
+def test_fibonacci_three():
+    actual = fibonacci(3)
+    expected = 1
+    assert actual == expected
+def test_fibonacci_four():
+    actual = fibonacci(4)
+    expected = 2
+    assert actual == expected
+def test_fibonacci_five():
+    actual = fibonacci(5)
+    expected = 3
+    assert actual == expected
+def test_fibonacci_six():
+    actual = fibonacci(6)
+    expected = 5
+    assert actual == expected
+
+#Lucas Tests
+def test_lucas_one():
+    actual = lucas(1)
+    expected = 2
+    assert actual == expected
+def test_lucas_two():
+    actual = lucas(2)
+    expected = 1
+    assert actual == expected
+def test_lucas_three():
+    actual = lucas(3)
+    expected = 3
+    assert actual == expected
+def test_lucas_four():
+    actual = lucas(4)
+    expected = 4
+    assert actual == expected
+def test_lucas_five():
+    actual = lucas(5)
+    expected = 7
+    assert actual == expected
+def test_lucas_six():
+    actual = lucas(6)
+    expected = 11
+    assert actual == expected
+
+#Sum series tests
+def test_sumseries_fib_one():
+    actual = sum_series(1)
+    expected = 0
+    assert actual == expected
+def test_sumseries_fib_one1():
+    actual = sum_series(1,0,1)
+    expected = 0
+    assert actual == expected
+def test_sumseries_fib_two1():
+    actual = sum_series(2,0,1)
+    expected = 1
+    assert actual == expected
+def test_sumseries_fib_two():
+    actual = sum_series(2)
+    expected = 1
+    assert actual == expected
+def test_sumseries_fib_three():
+    actual = sum_series(3)
+    expected = 1
+    assert actual == expected
+def test_sumseries_fib_four():
+    actual = sum_series(4)
+    expected = 2
+    assert actual == expected
+def test_sumseries_lucas_one():
+    actual = sum_series(1,2,1)
+    expected = 2
+    assert actual == expected
+def test_sumseries_lucas_one1():
+    actual = sum_series(1,2,1)
+    expected = 2
+    assert actual == expected
+def test_sumseries_lucas_two():
+    actual = sum_series(2,2,1)
+    expected = 1
+    assert actual == expected
+def test_sumseries_lucas_three():
+    actual = sum_series(3,2,1)
+    expected = 3
+    assert actual == expected
+def test_sumseries_lucas_four():
+    actual = sum_series(4,2,1)
+    expected = 4
     assert actual == expected
