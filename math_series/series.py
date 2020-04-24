bc366e5720a4bf336426181b4fd6950dd39228d6
Shaik <ameeruns@f45c89abcadd.ant.amazon.com>
2020-04-23 22:03:27 -0700

    PushTheChange

--- math_series/series.py
+++ math_series/series.py
@@ -30,3 +30,35 @@ def sum_series(num_check,optional_one=0,optional_two=1):
        return lucas(num_check)
     else:
         print("Incorrect parameter") 
+
+
+def fibonacci_parameters(input_num,option_1,option_2): 
+ if input_num < 0: 
+  print("Incorrect input") 
+# First Fibonacci number is 0 
+ elif input_num==1: 
+    return 0
+# Second Fibonacci number is 1 
+ elif input_num==2: 
+   return 1
+ else: 
+   return fibonacci_parameters(input_num-1,option_1,option_2)+fibonacci_parameters(input_num-2,option_1,option_2) 
+
+def lucas_parameters(input_num,option_1,option_2): 
+ if input_num < 0: 
+  print("Incorrect input") 
+# First Lucas number is 2
+ elif input_num==1: 
+    return 2
+# Second Lucas number is 1 
+ elif input_num==2: 
+   return 1
+ else: 
+   return lucas_parameters(input_num-1,option_1,option_2)+lucas_parameters(input_num-2,option_1,option_2) 
+def sum_series_param(num_check,optional_one=0,optional_two=1):
+    if optional_one == 0 and optional_two == 1:
+       return fibonacci_parameters(num_check,optional_one,optional_two)
+    elif optional_one == 2 and optional_two == 1:
+       return lucas_parameters(num_check,optional_one,optional_two)
+    else:
+        print("Incorrect parameter") 
--- tests/test_math_series.py
+++ tests/test_math_series.py
@@ -2,6 +2,9 @@ from math_series import __version__
 from math_series.series import fibonacci
 from math_series.series import lucas
 from math_series.series import sum_series
+from math_series.series import fibonacci_parameters
+from math_series.series import lucas_parameters
+from math_series.series import sum_series_param
 
 def test_version():
     assert __version__ == "0.1.0"
@@ -100,4 +103,67 @@ def test_sumseries_lucas_three():
 def test_sumseries_lucas_four():
     actual = sum_series(4,2,1)
     expected = 4
+    assert actual == expected
+#Fibonacci with parameters
+def test_fibonacci_parameters_one():
+    actual = fibonacci_parameters(1,0,1)
+    expected = 0
+    assert actual == expected
+def test_fibonacci_parameters_two():
+    actual = fibonacci_parameters(2,0,1)
+    expected = 1
+    assert actual == expected
+def test_fibonacci_parameters_three():
+    actual = fibonacci_parameters(3,0,1)
+    expected = 1
+    assert actual == expected
+def test_fibonacci_parameters_four():
+    actual = fibonacci_parameters(4,0,1)
+    expected = 2
+    assert actual == expected
+def test_fibonacci_parameters_five():
+    actual = fibonacci_parameters(5,0,1)
+    expected = 3
+    assert actual == expected
+#Lucas parameter Tests
+def test_lucas_param_one():
+    actual = lucas_parameters(1,2,1)
+    expected = 2
+    assert actual == expected
+def test_lucas_param_two():
+    actual = lucas_parameters(2,2,1)
+    expected = 1
+    assert actual == expected
+def test_lucas_param_three():
+    actual = lucas_parameters(3,2,1)
+    expected = 3
+    assert actual == expected
+def test_lucas_param_four():
+    actual = lucas_parameters(4,2,1)
+    expected = 4
+    assert actual == expected
+def test_lucas_param_five():
+    actual = lucas_parameters(5,2,1)
+    expected = 7
+    assert actual == expected
+def test_lucas_param_six():
+    actual = lucas_parameters(6,2,1)
+    expected = 11
+    assert actual == expected
+#Sum series parameter tests
+def test_sumseries_fib_param_one():
+    actual = sum_series_param(1)
+    expected = 0
+    assert actual == expected
+def test_sumseries_fib_param_one1():
+    actual = sum_series_param(1,0,1)
+    expected = 0
+    assert actual == expected
+def test_sumseries_fib_param_two1():
+    actual = sum_series_param(2,0,1)
+    expected = 1
+    assert actual == expected
+def test_sumseries_fib_param_two():
+    actual = sum_series_param(2)
+    expected = 1
     assert actual == expected
